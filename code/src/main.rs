6294270f09f1080306ce5d55a61e3e270142befa
tucker <tmantsmith@gmail.com>
2025-09-24 16:39:40 -0400

    rust pracice

--- code/src/main.rs
+++ code/src/main.rs
@@ -1,35 +1,39 @@
 fn main() {
-    let mut x = 0;
-    let mut y = 0;
+    let mut x = String::new();
+    let mut y = String::new();
+    let mut input = String::new();
     let mut opp = String::new();
 
     println!("number a:");
-    std::io::stdin().read_line(&mut x);
-    
+    std::io::stdin().read_line(&mut input);
+    x = input.trim().parse();
+
     println!("number b:");
-    std::io::stdin().read_line(&mut y);
-    
+    std::io::stdin().read_line(&mut input);
+    y = input.trim().parse();
+
+
     println!("what operation do you want to preform");
     std::io::stdin().read_line(&mut opp);
 
     if opp == "+"{
         x +=y;
-        println!("This is the answer {x}");
+        println!("This is the answer {}", x);
     } else if opp == "-" {
         x -= y;
-        println!("This is the answer {x}");
+        println!("This is the answer {}", x);
     } else if opp == "*" {
         x *= y;
-        println!("This is the answer {x}");
+        println!("This is the answer {}", x);
     } else if opp == "/" {
         if y == 0 {
             println!("divide by 0 error");
         }
         else {
             x /= y;
-            println!("This is the answer {x}");
+            println!("This is the answer {}", x);
         }
     } else {
-        println!("{opp} is not a valid opporator please use +, -, *, or /");
+        println!("{} is not a valid opporator please use +, -, *, or /", opp);
     }   
 }
