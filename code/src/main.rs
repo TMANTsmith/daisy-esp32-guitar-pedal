eceaf5955455749ed2453934a925220946d989ec
tucker <tmantsmith@gmail.com>
2025-09-25 19:05:25 -0400

    learned how to use rust fuctions

--- code/src/main.rs
+++ code/src/main.rs
@@ -1,37 +1,41 @@
-fn main() {
-    let mut input2 = String::new();
+
+fn userInt() -> i32 {
     let mut input1 = String::new();
+    
+    let _ =  std::io::stdin().read_line(&mut input1);
+    let mut x: i32 = input1.trim().parse().expect("failed to parse");
+    return x;
+}
+
+fn main() {
     let mut opp = String::new();
 
     println!("number a:");
-    let _ = std::io::stdin().read_line(&mut input1);
-    let mut x: i32 = input1.trim().parse().expect("failed to parse");
-
+    let mut num1: i32 = userInt();
+    
     println!("number b:");
-    let _ = std::io::stdin().read_line(&mut input2);
-    let mut y: i32 = input2.trim().parse().expect("failed to parse");
-
+    let mut num2: i32 = userInt();
 
     println!("what operation do you want to preform");
     let _ = std::io::stdin().read_line(&mut opp).unwrap();
     let opp = opp.trim(); // <- key line
-     
+
     if opp == "+"{
-        x +=y;
-        println!("This is the answer {}", x);
+        num1 += num2;
+        println!("This is the answer {}", num1);
     } else if opp == "-" {
-        x -= y;
-        println!("This is the answer {}", x);
+        num1 -= num2;
+        println!("This is the answer {}", num1);
     } else if opp == "*" {
-        x *= y;
-        println!("This is the answer {}", x);
+        num1 *= num2;
+        println!("This is the answer {}", num1);
     } else if opp == "/" {
-        if y == 0 {
+        if num2 == 0 {
             println!("divide by 0 error");
         }
         else {
-            x /= y;
-            println!("This is the answer {}", x);
+            num1 /= num2;
+            println!("This is the answer {}", num1);
         }
     } else {
         println!("this is not a valid opporator please use +, -, *, or /");
