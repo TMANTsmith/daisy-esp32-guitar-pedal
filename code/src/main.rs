a74595ea10fd7425466c1b59c6124423c081b89c
tucker <tmantsmith@gmail.com>
2025-09-25 17:41:08 -0400

    working calc

--- code/src/main.rs
+++ code/src/main.rs
@@ -1,21 +1,21 @@
 fn main() {
-    let mut x = String::new();
-    let mut y = String::new();
-    let mut input = String::new();
+    let mut input2 = String::new();
+    let mut input1 = String::new();
     let mut opp = String::new();
 
     println!("number a:");
-    std::io::stdin().read_line(&mut input);
-    x = input.trim().parse();
+    let _ = std::io::stdin().read_line(&mut input1);
+    let mut x: i32 = input1.trim().parse().expect("failed to parse");
 
     println!("number b:");
-    std::io::stdin().read_line(&mut input);
-    y = input.trim().parse();
+    let _ = std::io::stdin().read_line(&mut input2);
+    let mut y: i32 = input2.trim().parse().expect("failed to parse");
 
 
     println!("what operation do you want to preform");
-    std::io::stdin().read_line(&mut opp);
-
+    let _ = std::io::stdin().read_line(&mut opp).unwrap();
+    let opp = opp.trim(); // <- key line
+     
     if opp == "+"{
         x +=y;
         println!("This is the answer {}", x);
@@ -34,6 +34,6 @@ fn main() {
             println!("This is the answer {}", x);
         }
     } else {
-        println!("{} is not a valid opporator please use +, -, *, or /", opp);
+        println!("this is not a valid opporator please use +, -, *, or /");
     }   
 }
