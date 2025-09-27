7a403e2535bf6e4550ba0a1e8addb385ed17b38d
tucker <tmantsmith@gmail.com>
2025-09-27 16:19:39 -0400

    im tired of trying to make something i will not use

--- code/src/main.rs
+++ code/src/main.rs
@@ -6,6 +6,20 @@ fn main(){
     use std::slice;
 
     while in_use == true {
+        if task_list.len() > 0 {
+            for i in 0..task_list.len(){
+                println!("Name: {}", task_list[i].name);
+                println!("discription: {}", task_list[i].discription);
+                println!("completed: {}", task_list[i].completed);
+                println!("------------------------------------");
+            }
+        } else {
+            println!("No tasks added yet");
+        }
+
+
+
+        println!("------------------------------------");
         println!("enter a command");
         println!("1. add a task");
         println!("2. delete a task");
@@ -13,15 +27,23 @@ fn main(){
         println!("4. change status");
         std::io::stdin().read_line(&mut command);
 
-        match command.as_str() {
+        println!("you entered {}", &command);
+
+        match command.trim() {
             "1" => task_list = addtask(task_list),
             "2" => task_list = deletetask(task_list),
             "3" => task_list = changetask(task_list),
-            "4" => task_list = changestatus(task_list),
+            "4" =>{ if task_list.len() == 0 {
+                println!("there is nothing to delete");
+                } else { 
+                    task_list = changestatus(task_list);
+                }
+            }
             _ => {
                 println!("Please enter a number 1-4");
             },
         }
+        command.clear();
 
 
     }
@@ -37,24 +59,31 @@ fn addtask(mut list : Vec<Task>) -> Vec<Task> {
     let mut discription_temp = String::new();
     let mut completed_temp = String::new();
     let mut completed_bool : bool = false;
-
+    let mut selected : bool = false;
 
     println!("What do you want to call the item?");
     std::io::stdin().read_line(&mut name_temp);
    
     println!("What discription do you want it to have?");
     std::io::stdin().read_line(&mut discription_temp);
-   
-    println!("Is this item completed? yes/no");
-    std::io::stdin().read_line(&mut completed_temp);
-    if completed_temp == "yes"{
-        completed_bool = true;
-    }
-    else if completed_temp == "no" {
-        completed_bool = false;
-    }
-    else {
-        println!("Please enter yes or no.");
+
+    while selected == false {
+
+        println!("Is this item completed? yes/no");
+        std::io::stdin().read_line(&mut completed_temp);
+
+        if completed_temp.trim() == "yes"{
+            completed_bool = true;
+            selected = true;
+        }
+        else if completed_temp.trim() == "no" {
+            completed_bool = false;
+            selected = true;
+        }
+        else {
+            println!("Please enter yes or no.");
+            completed_temp.clear()
+        }
     }
 
     let task_temp = Task {
@@ -62,33 +91,40 @@ fn addtask(mut list : Vec<Task>) -> Vec<Task> {
         discription : discription_temp,
         completed : completed_bool,
     };
+    
+    println!("You task looks like this:");
+    println!("Name: {}", task_temp.name);
+    println!("discription: {}", task_temp.discription);
+    println!("completed: {}", task_temp.completed);
+    println!("------------------------------------");
+
     list.push(task_temp);
     return list
 }
-fn deletetask(list : Vec<Task>) -> Vec<Task> {
+fn deletetask(mut list : Vec<Task>) -> Vec<Task> {
     println!("What task do you want to delete");
-    let deletion = String::new();
+    let mut deletion = String::new();
+    let mut chosen : bool = false;
+    while chosen == false {
 
-    for i in slice::range(list.len()) {
-        i += 1;
-        let mut chosen : bool = false;
+        for i in 0..list.len(){
 
-        while chosen == false; {
-            println!("{}. {}" i, list[(i-1)]);
+            println!("{}. {}", i + 1, list[i].name);
+        }
             std::io::stdin().read_line(&mut deletion);
-            if deletion.parce() <= 0 {
+            let index_deleted : u32 = deletion.parse::<u32>().expect("Please enter a valid number");
+
+            if index_deleted <= 0 {
                 println!("Please enter a number from the list");
                 deletion.clear();
             }
             else {
-                chosen == true;
+                let removed_index : usize = index_deleted as usize - 1 as usize;
+                println!("Removed {}", list[removed_index].name);
+                list.remove(removed_index);
+                chosen = true;
             }
         }
-        i -= 1;
-    }
-    deletion = ((deletion.parse()) - 1);
-
-    list.remove(deletion);
     return list
 }
 fn changetask(list : Vec<Task>) -> Vec<Task> {
